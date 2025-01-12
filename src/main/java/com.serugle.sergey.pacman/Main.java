b2aa2a66a907fa9bea6678df4e98b4b36df12bdc
duhov <portoslunt@gmail.com>
2025-01-12 18:56:36 +0300

    draft

--- src/main/java/com.serugle.sergey.pacman/Main.java
+++ src/main/java/com.serugle.sergey.pacman/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.serugle.sergey.pacman;
 
 import javax.swing.*;
 
--- src/main/java/com.serugle.sergey.pacman/Model.java
+++ src/main/java/com.serugle.sergey.pacman/Model.java
@@ -1,5 +1,5 @@
 
-package org.example;
+package com.serugle.sergey.pacman;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
