9fbc680266d691488d070eea393d609ea1f5c11b
duhov <portoslunt@gmail.com>
2025-01-12 18:51:14 +0300

    draft

---
+++ src/main/java/com.serugle.sergey.pacman/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        Main pac = new Main();
+        pac.setVisible(true);
+        pac.setTitle("Pacman");
+        pac.setSize(380, 420);
+        pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pac.setLocationRelativeTo(null);
+    }
+}
